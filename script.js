48b160fa08b8b26f061f794a536c344de942e6a4
Dante O. <verlacdante@gmail.com>
2024-07-03 11:25:04 -0300

    Added spinner while the page loads.

--- script.js
+++ script.js
@@ -2,12 +2,18 @@ const days = document.getElementById('days');
 const hours = document.getElementById('hours');
 const minutes = document.getElementById('minutes');
 const seconds = document.getElementById('seconds');
-
 const countdown = document.getElementById('countdown');
+const loading = document.getElementById('loading');
+
+const year = document.getElementById('year');
 
 const currentYear = new Date().getFullYear();
 
 const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
+
+// set background year
+year.innerText = currentYear + 1;
+
 
 function updateCountdown() {
     const currentTime = new Date();
@@ -26,4 +32,11 @@ function updateCountdown() {
     seconds.innerHTML = s < 10 ? '0' + s : s;
 }
 
+//show spinner before countdown
+setTimeout(() => {
+    loading.remove();
+    countdown.style.display = 'flex';
+}, 1000);
+
+// run every second
 setInterval(updateCountdown, 1000);
